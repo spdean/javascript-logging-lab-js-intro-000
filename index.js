5ce0260bfad7570532504af39043f0ad41311089
Sylvia Dean <orangespd@gmail.com>
2020-02-26 00:59:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
 console.error("HALP!")
 console.log("I would be a logger.")
 console.warn("Keep your socks on!")
+=======
+console.error(HALP!)
+>>>>>>> 5b36c07b407144d114ccaa6ee9861fb7adfdec72
